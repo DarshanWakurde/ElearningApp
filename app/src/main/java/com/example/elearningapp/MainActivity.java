a1b19472649bf88a328de4eb2d59d160932938dd
Darshan Wakurde <wakurdedarshan@gmail.com>
2023-10-01 16:03:25 +0530

    try comment

--- app/src/main/java/com/example/elearningapp/MainActivity.java
+++ app/src/main/java/com/example/elearningapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//new one addeed here
         FragmentManager fm=getSupportFragmentManager();
         FragmentTransaction fragtrran=fm.beginTransaction().add(R.id.myFrame,RegisterFragment.class,null);
         fragtrran.commit();
