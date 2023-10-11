d9b75eb5e56b18b38765104614a65aa3b397b0fd
Bajrang More <bajrangmore272001@gmail.com>
2023-10-12 01:27:45 +0530

    Ui Rebuilds

--- app/src/main/java/com/example/elearningapp/Nextactivity.java
+++ app/src/main/java/com/example/elearningapp/Nextactivity.java
@@ -6,16 +6,22 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Nextactivity extends AppCompatActivity {
 
     ImageView Course_image;
     TextView name_Course;
+    ListView listView;
+    ArrayList<nextitem_models> items;
 
-    Button btn;
+    nextCustomadapter nextCustomadapter;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,20 +31,42 @@ public class Nextactivity extends AppCompatActivity {
 
         Course_image=findViewById(R.id.courseImage);
         name_Course=findViewById(R.id.nameCourse);
+        listView=findViewById(R.id.listview1);
+
+        items=new ArrayList<>();
+        nextitem_models title2=new nextitem_models(R.drawable.videoss,"Videos");
+        nextitem_models title3=new nextitem_models(R.drawable.quizz,"Quizz");
+        nextitem_models title4=new nextitem_models(R.drawable.notes,"Notes");
+        nextitem_models title5=new nextitem_models(R.drawable.papersm,"Question Papers");
+
+        items.add(title2);
+        items.add(title3);
+        items.add(title4);
+        items.add(title5);
+
+        nextCustomadapter=new nextCustomadapter(this,items);
+        listView.setAdapter(nextCustomadapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(items.get(i).getTitle1().equals("Videos")){
+                    startActivity(new Intent(Nextactivity.this,VideoList.class));
+                }
+            }
+        });
+
+
+
+
+
 
 
         Intent nextIntent=getIntent();
         name_Course.setText(nextIntent.getStringExtra("Name"));
         Course_image.setImageResource(nextIntent.getIntExtra("Resid",R.drawable.java));
 
-        btn=findViewById(R.id.video);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Nextactivity.this,VideoList.class));
-            }
-        });
 
 
 
