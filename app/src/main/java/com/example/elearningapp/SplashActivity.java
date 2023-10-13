d48aa09a0769f2e6cd9faccfe68d260e06c4bf36
Darshan Wakurde <wakurdedarshan@gmail.com>
2023-10-14 00:50:08 +0530

    Validations Complete

--- app/src/main/java/com/example/elearningapp/SplashActivity.java
+++ app/src/main/java/com/example/elearningapp/SplashActivity.java
@@ -5,14 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
+        imageView=findViewById(R.id.imgview);
+
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+// Apply the animation to the ImageView
+        imageView.startAnimation(fadeInAnimation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
