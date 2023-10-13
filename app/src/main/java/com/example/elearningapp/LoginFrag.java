77f54593c0d1ea71b70dd7638204d6528fcc07fa
Darshan Wakurde <wakurdedarshan@gmail.com>
2023-10-13 23:03:49 +0530

    Validations Complete

--- app/src/main/java/com/example/elearningapp/LoginFrag.java
+++ app/src/main/java/com/example/elearningapp/LoginFrag.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,9 +17,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LoginFrag extends Fragment {
@@ -42,19 +47,41 @@ public class LoginFrag extends Fragment {
         logpassword=view.findViewById(R.id.loginpass);
 
         mAuth=FirebaseAuth.getInstance();
+
+
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                if (isValidEmail(emlog.getText().toString())&&(!emlog.getText().toString().isEmpty())) {
 
-                mAuth.signInWithEmailAndPassword(emlog.getText().toString(),logpassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                            startActivity(new Intent(getActivity(),Courser.class));
-                        Toast.makeText(getActivity(),"login Successfully",Toast.LENGTH_LONG).show();
+                    if (isValidPassword(logpassword.getText().toString()) && (!logpassword.getText().toString().isEmpty())) {
+
+                        mAuth.signInWithEmailAndPassword(emlog.getText().toString(), logpassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(getActivity(), Courser.class));
+                                Toast.makeText(getActivity(), "login Successfully", Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Sorry incorrect Email or Password", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
-                });
+                    else {
+                        Toast.makeText(getActivity(), "Password id Wrong or empty", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(getActivity(), "Sorry Wrong Email or No Input", Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
 
         loginmi.setOnClickListener(new View.OnClickListener() {
@@ -75,5 +102,20 @@ public class LoginFrag extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+
+    public static boolean isValidEmail(String email) {
+        String EMAIL_REGEX = "^[\\w\\.-]+@[\\w\\.-]+\\.\\w+$";
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPassword(String password) {
+        String PASSWORD_REGEX = "^[a-zA-Z]{6,}$";
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
--- app/src/main/java/com/example/elearningapp/RegisterFragment.java
+++ app/src/main/java/com/example/elearningapp/RegisterFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -18,6 +19,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,6 +27,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class RegisterFragment extends Fragment {
@@ -64,17 +68,45 @@ public class RegisterFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
 
-                        int id = images[in.nextInt(images.length)];
-                        createUserFirebase(email.getText().toString(), name.getText().toString(), mobileno.getText().toString(), pass.getText().toString(), id);
+                if(isValidEmail(email.getText().toString())&&(!email.getText().toString().isEmpty())) {
+                    if (isValidPassword(pass.getText().toString()) && (!pass.getText().toString().isEmpty())) {
 
-                        Toast.makeText(getActivity(), "Created Successfully", Toast.LENGTH_LONG).show();
-                        fragTransc.commit();
+                        if (isValidMobileNumber(mobileno.getText().toString()) && (!mobileno.getText().toString().isEmpty())) {
+
+                            mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+
+                                    int id = images[in.nextInt(images.length)];
+                                    createUserFirebase(email.getText().toString(), name.getText().toString(), mobileno.getText().toString(), pass.getText().toString(), id);
+
+                                    Toast.makeText(getActivity(), "Created Successfully", Toast.LENGTH_LONG).show();
+                                    fragTransc.commit();
+                                }
+
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getActivity(), "Sorry Email Alredy Exist", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(getActivity(), "Mobile Number is Not Valid", Toast.LENGTH_LONG).show();
+                        }
+
+                    }else {
+                        Toast.makeText(getActivity(), "Not Valid password", Toast.LENGTH_LONG).show();
+
                     }
-                });
+                }
+                else {
+                    Toast.makeText(getActivity(), "Not Valid Email", Toast.LENGTH_LONG).show();
+
+                }
+
+
             }
         });
 
@@ -116,5 +148,28 @@ public class RegisterFragment extends Fragment {
                             getActivity().finish();
         }
 
+    }
+
+
+    public static boolean isValidEmail(String email) {
+        String EMAIL_REGEX = "^[\\w\\.-]+@[\\w\\.-]+\\.\\w+$";
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPassword(String password) {
+        String PASSWORD_REGEX = "^[a-zA-Z]{6,}$";
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+
+    public static boolean isValidMobileNumber(String phoneNumber) {
+        String MOBILE_NUMBER_REGEX = "^(\\+\\d{1,3}[- ]?)?\\d{10}$";
+        Pattern pattern = Pattern.compile(MOBILE_NUMBER_REGEX);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
 }
