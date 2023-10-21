5b53f15c5fc3451a8796da57de9cafe74704c2fe
Darshan Wakurde <wakurdedarshan@gmail.com>
2023-10-22 01:12:14 +0530

    Progress bar updated

--- app/src/main/java/com/example/elearningapp/SplashActivity.java
+++ app/src/main/java/com/example/elearningapp/SplashActivity.java
@@ -9,16 +9,27 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.example.loadinganimation.LoadingAnimation;
+
 public class SplashActivity extends AppCompatActivity {
 
     ImageView imageView;
+
+    LoadingAnimation loadingAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        loadingAnim=findViewById(R.id.loadingAnim);
+        loadingAnim.setProgressVector(getResources().getDrawable(R.drawable.kkkkk));
+        loadingAnim.setTextViewVisibility(true);
+        loadingAnim.setTextStyle(true);
+        loadingAnim.setTextColor(R.color.background);
+        loadingAnim.setTextSize(12F);
+        loadingAnim.setTextMsg("Please Wait");
+        loadingAnim.setEnlarge(5);
         imageView=findViewById(R.id.imgview);
 
         Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
